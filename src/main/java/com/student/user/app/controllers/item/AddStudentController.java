962d3f0ee7db127c4fccc0b432cc113633544bf4
steven <stevenrabacal2@gmail.com>
2024-05-07 20:01:56 +0800

    RCPS-199:stud-info-mgt-app: ALL PAGES(except EDITUSER page)-fix all email fields

--- src/main/java/com/student/user/app/controllers/item/AddStudentController.java
+++ src/main/java/com/student/user/app/controllers/item/AddStudentController.java
@@ -99,7 +99,7 @@ public class  AddStudentController implements Initializable {
         Map<String, String> invalidFields = getInvalidFields();
         String emailInput = email.getText();
         if (!isValidEmail(emailInput)) {
-            showAlert("Invalid Email Address", " Invalid Email Address. and not accept email without “@gmail.com“.");
+            showAlert("Invalid Email Address", " Invalid Email Address. and not accept email without “@“ “.com“.");
             return;
         }
         if (!invalidFields.isEmpty()) {
@@ -161,7 +161,7 @@ public class  AddStudentController implements Initializable {
         }
     }
     private boolean isValidEmail(String email) {
-        return email.toLowerCase().endsWith("@gmail.com");
+        return email != null && (email.toLowerCase().endsWith("@gmail.com") || email.toLowerCase().endsWith(".com")) && email.contains("@");
     }
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
